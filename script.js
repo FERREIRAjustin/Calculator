177411ba72baf0738d419ae289c4c3f666b97e91
FERREIRAjustin <justin.ferreirasimoes@gmail.com>
2023-03-26 09:24:32 +0100

    V0.2

--- script.js
+++ script.js
@@ -7,3 +7,19 @@ function insert(num) {
   calculation.textContent += num;
   console.log(num);
 }
+
+document.getElementById("clear").addEventListener("click", function () {
+  calculation.textContent = "";
+  result.textContent = 0;
+});
+
+document.getElementById("back").addEventListener("click", function () {
+  var exp = calculation.textContent;
+  calculation.textContent = exp.substring(0, exp.length-1);
+});
+
+document.getElementById("equal").addEventListener("click", function () {
+  var exp = calculation.textContent;
+  result.textContent = eval(exp);
+  calculation.textContent = result.textContent;
+});
